2ff7dee9ce2033992b9a723f6c1c06c76a8691ef
Souvik Chatterjee <91524372+Bravoth0729v@users.noreply.github.com>
2021-12-12 09:30:38 +0530

    Add files via upload

---
+++ Souvik chatterjee.py
@@ -0,0 +1,6 @@
+username = input("Enter Your Name: ")
+print("Your Username Is: "+username)
+list = ['souvik','rahul','ronit']
+print("Your List Is: ",list)
+list[1] = 'pallab'
+print("The Changed List Is: ",list)
